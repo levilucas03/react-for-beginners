c80e8f07f85bb867c0d4ae17b3ebbe48882bbad3
Levi Lucas <levi.lucas@asset.tv>
2018-03-09 15:55:02 +0000

    Added in first Comp
    Added in fragment to allow for many div instad of inside one example

---
+++ src/components/StorePicker.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Fragment} from 'react'
+
+class StorePicker extends React.Component {
+
+    render() {
+        return (
+            <Fragment>
+                <p>So Far so good</p>
+                <form action="" className="store-selector">
+                    <h2>Please Enter A Store</h2>
+                    <input required type="text" placeholder="Store Name" />
+                    <button type="submit">Visist Store</button>
+                </form>
+            </Fragment>
+        )
+    }
+}
+
+export default StorePicker;
